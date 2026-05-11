ba8545a6c9f1ac7d947606836f02da557fd2ec3f
prakashwaje123 <prakashwaje123@gmail.com>
2026-05-11 09:23:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,24 @@
-const CACHE_NAME = "v2"; // Changing this from 'v1' to 'v2' tells the browser there is an update
-const ASSETS_TO_CACHE = [
+// 1. Import OneSignal (This merges both workers into one)
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+const CACHE_NAME = "v3";
+const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json",
-  "./OneSignalSDKWorker.js"
+  "./manifest.json"
 ];
 
+// Install Event
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting(); // Forces the new service worker to become active immediately
+  self.skipWaiting();
 });
 
+// Activate Event
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -27,6 +29,7 @@ self.addEventListener("activate", (event) => {
   );
 });
 
+// MANDATORY: Fetch listener for "Install App" prompt
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
